e8db9aaa1a67d36aef9bf8b77689b348eb83516d
Isvvc <lyons@tuta.io>
2021-01-27 19:02:59 -0700

    Correct file path to not include path to dav
    For example, remove the /remote.php/dav/... from the path for a Nextcould server.

--- Sources/WebDAV/WebDAVFile.swift
+++ Sources/WebDAV/WebDAVFile.swift
@@ -10,6 +10,8 @@ import SWXMLHash
 
 public class WebDAVFile: NSObject, Identifiable {
     
+    //MARK: Properties
+    
     public var path: String
     public var id: String
     public var isDirectory: Bool
@@ -27,9 +29,9 @@ public class WebDAVFile: NSObject, Identifiable {
         self.etag = etag
     }
     
-    convenience init?(xml: XMLIndexer) {
+    convenience init?(xml: XMLIndexer, baseURL: String?) {
         let properties = xml["propstat"][0]["prop"]
-        guard let path = xml["href"].element?.text,
+        guard var path = xml["href"].element?.text,
               let dateString = properties["getlastmodified"].element?.text,
               let date = WebDAVFile.rfc1123Formatter.date(from: dateString),
               let id = properties["fileid"].element?.text,
@@ -38,14 +40,36 @@ public class WebDAVFile: NSObject, Identifiable {
               let etag = properties["getetag"].element?.text else { return nil }
         let isDirectory = properties["getcontenttype"].element?.text == nil
         
+        if let baseURL = baseURL {
+            path = WebDAVFile.removing(endOf: baseURL, from: path)
+        }
+        
         self.init(path: path, id: id, isDirectory: isDirectory, lastModified: date, size: size, etag: etag)
     }
+    
+    //MARK: Static
     
     static let rfc1123Formatter: DateFormatter = {
         let formatter = DateFormatter()
         formatter.dateFormat = "EEE, dd MMM yyyy HH:mm:ss z"
         return formatter
     }()
+    
+    private static func removing(endOf string1: String, from string2: String) -> String {
+        guard let first = string2.first else { return string2 }
+        
+        for (i, c) in string1.enumerated() {
+            guard c == first else { continue }
+            let end = string1.dropFirst(i)
+            if string2.hasPrefix(end) {
+                return String(string2.dropFirst(end.count))
+            }
+        }
+        
+        return string2
+    }
+    
+    //MARK: Public
     
     public override var description: String {
         path
